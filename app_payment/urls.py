51276fb4cc2e0b0bcc40742e1886837e6d1cf75d
zubizhenya <e.zubilin@yandex.ru>
2023-08-14 14:58:09 +0700

    shop

---
+++ app_payment/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+from .views import payment
+
+urlpatterns = [path("new/", payment, name="pay_new")]
---
+++ app_shop/utils_shop.py
@@ -0,0 +1,40 @@
+from contextlib import suppress
+
+from django.db.models import Min, Max, QuerySet
+from .models import Product, Category
+
+
+def get_data_min(root_category: Category, queryset: QuerySet = None) -> int:
+    min_price = None
+    if queryset:
+        min_price = queryset.values("price").aggregate(Min("price"))["price__min"]
+    else:
+        with suppress(AttributeError):
+            sub_categories = root_category.get_descendants(include_self=True)
+            min_price = (
+                Product.objects.values("price")
+                .filter(category__in=sub_categories)
+                .filter(available=True)
+                .aggregate(Min("price"))["price__min"]
+            )
+        if not min_price:
+            return 0
+    return int(min_price)
+
+
+def get_data_max(root_category, queryset: QuerySet = None) -> int:
+    max_price = None
+    if queryset:
+        max_price = queryset.values("price").aggregate(Max("price"))["price__max"]
+    else:
+        with suppress(AttributeError):
+            sub_categories = root_category.get_descendants(include_self=True)
+            max_price = (
+                Product.objects.values("price")
+                .filter(category__in=sub_categories)
+                .filter(available=True)
+                .aggregate(Max("price"))["price__max"]
+            )
+        if not max_price:
+            return 0
+    return max_price
